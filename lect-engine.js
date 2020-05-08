d1eae8fc2b8d68d084e2aa11acf6c7624a06b309
AlvaroDeLaRosa <49072538+AlvaroDeLaRosa@users.noreply.github.com>
2020-05-07 20:32:29 -0500

    Filtros

--- lect-engine.js
+++ lect-engine.js
@@ -1,10 +1,171 @@
 const op=document.getElementById('orden');
+const de=document.getElementById('date');
+const mn=document.getElementById('Mi');
+const mx=document.getElementById('Ma');
 const cont=document.getElementById('gallery');
+var obras=[];
 
 op.addEventListener('change', sort);
+de.addEventListener('change', priceFilter);
+mn.addEventListener('change', priceFilter);
+mx.addEventListener('change', priceFilter);
+
+function priceFilter() {
+    var minimo;
+    var maximo;
+    arr=[];
+    obras=JSON.parse(localStorage.getItem('products'));
+    console.log(obras);
+    if(mn.value=='') {
+        minimo=0;
+    }
+    else {
+        minimo=mn.value;
+    }
+    if(mx.value=='') {
+        maximo=100000000;
+    }
+    else {
+        maximo=mx.value;
+    }
+    for(c=0;c<obras.length;c++) {
+        if(obras[c].precio>=minimo && obras[c].precio<=maximo) {
+            arr.push(obras[c]);
+        }
+    }
+    obras=arr;
+    dateFilter();
+}
+
+function dateFilter() {
+    switch (de.value) {
+        case '0':
+            insertion(obras);
+            break;
+        case '1':
+            var f=new Date();
+            arr=[];
+            for(c=0;c<obras.length;c++) {
+                if(obras[c].fechaAlta.substr(0, 4)==String(f.getFullYear())) {
+                    var m;
+                    if((f.getMonth()+1)<10) {
+                        m='0'+String(f.getMonth()+1);
+                    }
+                    else {
+                        m=String(f.getMonth()+1);
+                    }
+                    if(obras[c].fechaAlta.substr(5, 2)==m) {
+                        var d;
+                        if((f.getMonth()+1)<10) {
+                            d='0'+String(f.getDate());
+                        }
+                        else {
+                            d=String(f.getDate());
+                        }
+                        if(obras[c].fechaAlta.substr(8, 2)==d) {
+                            arr.push(obras[c]);
+                        }
+                    }
+                }
+            }
+            obras=arr;
+            insertion(arr);
+            break;
+        case '2':
+            var f=new Date();
+            w=f.getDay();
+            arr=[];
+            days=[];
+            y=f.getFullYear();
+            m=f.getMonth()+1;
+            d=f.getDate();
+            for(x=w;x>0;x--) {
+                date=String(y);
+                if(m<10) {
+                    date+='-0'+String(m);
+                }
+                else {
+                    date+='-'+String(m);
+                }
+                if(d<10) {
+                    date+='-0'+String(d);
+                }
+                else {
+                    date+='-'+String(d);
+                }
+                days.push(date);
+                d--;
+                if(d<1) {
+                    m--;
+                    if(m==4 || m==6 || m==9 || m==11) {
+                        d=30;
+                    }
+                    else {
+                        if(m==2) {
+                            if(y%4==0) {
+                                d=29;
+                            }
+                            else {
+                                d=28;
+                            }
+                        }
+                        else {
+                            d=31;
+                        }
+                    }
+                    if(m<1) {
+                        m=12;
+                        y--;
+                    }
+                }
+            }
+            for(c=0;c<obras.length;c++) {
+                r=false;
+                i=0;
+                while(!r && i<days.length) {
+                    if(obras[c].fechaAlta.substr(0, 10)==days[i]) {
+                        arr.push(obras[c]);
+                        r=true;
+                    }
+                    i++;
+                }
+            }
+            obras=arr;
+            insertion(arr);
+            break;
+        case '3':
+            var f=new Date();
+            var m='';
+            arr=[];
+            if((f.getMonth()+1)<10) {
+                m='0'+String(f.getMonth()+1);
+            }
+            else {
+                m=String(f.getMonth()+1);
+            }
+            for(c=0;c<obras.length;c++) {
+                if(obras[c].fechaAlta.substr(5, 2)==m) {
+                    arr.push(obras[c]);
+                }
+            }
+            obras=arr;
+            insertion(arr);
+            break;
+        case '4':
+            var f=new Date();
+            arr=[];
+            for(c=0;c<obras.length;c++) {
+                if(obras[c].fechaAlta.substr(0, 4)==f.getFullYear()) {
+                    arr.push(obras[c]);
+                }
+            }
+            obras=arr;
+            insertion(arr);
+            break;
+    }
+}
 
 function sort() {
-    obras=JSON.parse(localStorage.getItem('products'));
     switch (op.value) {
         case '0':
             insertion(obras);
