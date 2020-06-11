501f171c9cae66237b14fdfd87699a647e63da5f
AlvaroDeLaRosa <49072538+AlvaroDeLaRosa@users.noreply.github.com>
2020-06-11 14:15:20 -0500

    Creación de usuarios

---
+++ pages/clientInsert.js
@@ -0,0 +1,122 @@
+const newClientData=document.getElementById('signIn');
+
+newClientData.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var form=new FormData(newClientData);
+    userID=localStorage.getItem('ID_User');
+    var bank=null;
+    var tarjeta=null;
+    var ready=true;
+    if(form.get('card')==null && form.get('paypal')==null) {
+        alert("Debes escoger al menos una forma de pago");
+    }
+    else {
+        if(form.get('card')=='on') {
+            bank=form.get('banco');
+            if(form.get('credit')==="") {
+                alert("Numero de tarjeta de credito/debito requerido");
+                ready=false;
+            }
+            else {
+                tarjeta=form.get('credit');
+            }
+        }
+        var user=null;
+        if(form.get('paypal')=='on') {
+            if(form.get('user')==="") {
+                alert("Usuario de PayPal requerido");
+                ready=false;
+            }
+            else {
+                user=form.get('user');
+                if(form.get('cont')==="") {
+                    alert("Contraseña de PayPal requerida");
+                    ready=false;
+                }
+            }
+        }
+        if(ready===true){  
+            var json={ 
+                "usuario_id": localStorage.getItem('ID_User'), 
+                "cuenta_paypal": user, 
+                "cuenta_tarjeta": tarjeta
+            };  
+            var xhr=new XMLHttpRequest();
+            xhr.withCredentials=true;
+            xhr.open("POST", "http://localhost:80/ejercicios-php/art_ss-master/clientes");
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(json));
+            xhr.addEventListener("readystatechange", function() {
+                var mes=JSON.parse(this.responseText);
+                if (mes.success===false){
+                    alert(mes.messages);
+                }
+                else {
+                    if(this.readyState===4) {
+                        if(form.get('card')=='on') {
+                            json={ 
+                                "usuario_id": userID, 
+                                "banco": bank, 
+                                "numero_cuenta": tarjeta
+                            };
+                            var xhrT=new XMLHttpRequest();
+                            xhrT.withCredentials=true;
+                            xhrT.open("POST", "http://localhost:80/ejercicios-php/art_ss-master/tarjetas");
+                            xhrT.setRequestHeader("Content-Type", "application/json");
+                            xhrT.send(JSON.stringify(json));
+                            xhrT.addEventListener("readystatechange", function() {
+                                mes=JSON.parse(this.responseText);
+                                if (mes.success===false){
+                                    alert(mes.messages);
+                                    ready=false;
+                                }
+                            });
+                        }
+                        if(ready===true) {
+                            var rol=localStorage.getItem('Rol');
+                            if(rol==2 || rol==4) {
+                                var xhrR=new XMLHttpRequest();
+                                xhrR.withCredentials=true;
+                                json={ 
+                                    "usuario_id": userID, 
+                                    "compras_id": null
+                                };
+                                xhrR.open("POST", "http://localhost:80/ejercicios-php/art_ss-master/compradores");
+                                xhrR.setRequestHeader("Content-Type", "application/json");
+                                xhrR.send(JSON.stringify(json));
+                                xhrR.addEventListener("readystatechange", function() {
+                                    mes=JSON.parse(this.responseText);
+                                    if (mes.success===false){
+                                        alert(mes.messages);
+                                        ready=false;
+                                    }
+                                });
+                            }
+                            if((rol==3 || rol==4) && ready===true) {
+                                xhr=new XMLHttpRequest();
+                                xhr.withCredentials=true;
+                                json={ 
+                                    "usuario_id": userID, 
+                                    "publicaciones_id": null
+                                };
+                                xhr.open("POST", "http://localhost:80/ejercicios-php/art_ss-master/vendedores");
+                                xhr.setRequestHeader("Content-Type", "application/json");
+                                xhr.send(JSON.stringify(json));
+                                xhr.addEventListener("readystatechange", function() {
+                                    mes=JSON.parse(this.responseText);
+                                    if (mes.success===false){
+                                        alert(mes.messages);
+                                        ready=false;
+                                    }
+                                });
+                            }
+                            if(ready===true) {
+                                location.href="index.html";
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+});
---
+++ pages/userInsert.js
@@ -0,0 +1,75 @@
+const newUserData=document.getElementById('signIn');
+const cred=document.getElementById('cred');
+const pay=document.getElementById('deb');
+
+newUserData.addEventListener('submit', function(e) {
+    e.preventDefault();
+    var f=new Date();
+    var fechaA=String(f.getFullYear())+"-";
+    if((f.getMonth()+1)<10) {
+        fechaA+="0";
+    }
+    fechaA+=String(f.getMonth()+1)+"-";
+    if(f.getDate()<10) {
+        fechaA+="0";
+    }
+    fechaA+=String(f.getDate())+" "+String(f.getHours())+":"+String(f.getMinutes());
+    var form=new FormData(newUserData);
+    var r=0;
+    switch(form.get('user-type')) {
+        case "user-buyer": 
+            r=2;
+            break;
+        case "user-seller":
+            r=3;
+            break;
+        case "user-buyandsell":
+            r=4;
+            break;
+    }
+    var idRol=0;
+    var xhrP=new XMLHttpRequest();
+    xhrP.withCredentials=true;
+    xhrP.open("POST", "http://localhost:80/ejercicios-php/art_ss-master/roles/"+r);
+    xhrP.addEventListener("readystatechange", function() {
+        if(this.readyState===4) {
+            var rol=JSON.parse(this.responseText);
+            idRol=rol.data.id;
+            //console.log(idRol);
+            var json={ 
+                "nombre": form.get('nombre'), 
+                "rol_id": idRol, 
+                "pais": form.get('pais'), 
+                "fecha_alta": fechaA, 
+                "calle": form.get('calle'), 
+                "colonia": form.get('colonia'), 
+                "numero_exterior": form.get('numero'), 
+                "codigo_postal": form.get('CP'), 
+                "estado": form.get('estado'), 
+                "status": "ACTIVO", 
+                "email": form.get('email'), 
+                "contrasena": form.get('contraseña'), 
+                "foto_perfil": null
+            };
+            var xhr=new XMLHttpRequest();
+            xhr.withCredentials=true;
+            xhr.open("POST", "http://localhost:80/ejercicios-php/art_ss-master/usuarios");
+            xhr.setRequestHeader("Content-Type", "application/json");
+            //console.log(json);
+            xhr.send(JSON.stringify(json));
+            xhr.addEventListener("readystatechange", function() {
+                var mes=JSON.parse(this.responseText);
+                console.log(mes);
+                if (mes.success===false){
+                    alert(mes.messages);
+                }
+                else {
+                    localStorage.setItem('ID_User', mes.data.id);
+                    localStorage.setItem('Rol', r);
+                    location.href="SignInVendedorAdvanced.html";
+                }
+            });
+        }
+    });
+    xhrP.send();
+})
