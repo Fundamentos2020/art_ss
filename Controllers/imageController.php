85470fd68c523991f56d343e0e47581e1c41f3ad
AlvaroDeLaRosa <49072538+AlvaroDeLaRosa@users.noreply.github.com>
2020-06-13 18:40:51 -0500

    Controller de imagenes arreglado

--- Controllers/imageController.php
+++ Controllers/imageController.php
@@ -6,19 +6,19 @@ require_once('../Models/Response.php');
 $response=new Response();
 
 try {
-    if($_SERVER['REQUEST_METHOD']==='POST' ) {
-        if( !isset( $_FILES['imagen']['error'] ) || is_array( $_FILES['imagen']['error'] ) )    {
+    if($_SERVER['REQUEST_METHOD']==='POST') {
+        if(!isset($_FILES['imagen']['error'] ) || is_array( $_FILES['imagen']['error'] ) )    {
             throw new Exception('Error en los parametros.', 400 );
         }
-        if( $_FILES['imagen']['error'] !== UPLOAD_ERR_OK )    {
+        if($_FILES['imagen']['error']!==UPLOAD_ERR_OK )    {
             $mensaje = 'Error al subir imagen.' . Imagen::ImagenUploadErrors[$_FILES['imagen']['error']] ;
             throw new Exception( $mensaje, 500 );
         }
         $nombre_imagen=$_FILES['imagen']['name'];
         $tmp=$_FILES['imagen']['tmp_name'];
-        move_uploaded_file($tmp, "Imagenes/".$nombre_imagen);
-        $response->setHttpStatusCode( 201 );
-        $response->setSuccess( true );
+        move_uploaded_file($tmp, "../images/".$nombre_imagen);
+        $response->setHttpStatusCode(201);
+        $response->setSuccess(true);
         $response->addMessage('Imagen subida.');
         $response->setData( $nombre_imagen );
         $response->send();
